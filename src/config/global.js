d4f413fe520a8236705e86f9deba188750baecdb
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-11-30 08:52:08 -0500

    Componente finalizado

--- src/config/global.js
+++ src/config/global.js
@@ -34,14 +34,20 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Generalidades del accidente de tránsito',
         desarrolloContenidos: true,
         subMenu: [
           {
             icono: 'far fa-file-alt',
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Marco jurídico del accidente de tránsito',
             hash: 't_1_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.2',
+            titulo: 'Accidente de solo daños materiales	',
+            hash: 't_1_2',
           },
         ],
       },
@@ -50,15 +56,63 @@ export default {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Accidentes de tránsito con heridos o muertos',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.1',
+            titulo: 'Aspectos normativos que regulan el accidente de tránsito',
+            hash: 't_2_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.2',
+            titulo:
+              'Procedimientos judiciales para desarrollar los actos urgentes	',
+            hash: 't_2_2',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Procedimientos Judiciales	',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.1',
+            titulo: 'Actuaciones del primer responsable',
+            hash: 't_3_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.2',
+            titulo: 'Actuaciones del coordinador	',
+            hash: 't_3_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.3',
+            titulo: 'Documentación topográfica',
+            hash: 't_3_3',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.4',
+            titulo: 'Documentación fotográfica',
+            hash: 't_3_4',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.5',
+            titulo:
+              'Documentación descriptiva y diligenciamiento de formatos de policía judicial (FPJ)',
+            hash: 't_3_5',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -103,31 +157,146 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: '1.2	Accidente de solo daños materiales',
+      referencia:
+        'Ley 769 de 2002. Por la cual se expide el Código Nacional de Tránsito Terrestre y se dictan otras disposiciones . Diario Oficial No. 44.932 de 13 de septiembre de 2002.',
+      tipo: 'Página web',
+      link:
+        'http://www.secretariasenado.gov.co/senado/basedoc/ley_0769_2002.html',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema:
+        '2.2. 	Procedimientos judiciales para desarrollar los actos urgentes.',
+      referencia:
+        'Ley 906 DE 2004. Por la cual se expide el Código de Procedimiento Penal. Diario Oficial No. 45.657 de 31 de agosto de 2004. ',
+      tipo: 'Página web',
+      link:
+        'http://www.secretariasenado.gov.co/senado/basedoc/ley_0906_2004.html',
+    },
+    {
+      tema:
+        '2.2. 	Procedimientos judiciales para desarrollar los actos urgentes',
+      referencia:
+        'Policía Nacional de Colombia. (2017). investigación accidentes de tránsito en Colombia. https://www.policia.gov.co/.',
+      tipo: 'Libro',
+      link:
+        'https://www.policia.gov.co/sites/default/files/publicaciones-institucionales/libro-investigacion-accidentes-de-transito-en-colombia.pdf',
+    },
+    {
+      tema: '3. 	Procedimientos Judiciales',
+      referencia: 'PROTOCOLOS FGN. (2022). polijudicial. ',
+      tipo: 'Página web',
+      link: 'https://oscarbasico.wixsite.com/polijudicial/protocolos-fgn',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Bosquejo',
+      significado:
+        'Representación gráfica general del lugar de los hechos, se caracteriza por ser a mano alzada, sin escala y se obtiene de manera inmediata en el lugar en donde se practiquen las diferentes diligencias judiciales.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Conciso',
+      significado:
+        'Esto se logra utilizando oraciones cortas, palabras usuales y breves sin sacrificar detalles de exactitud, integridad o claridad.',
+    },
+    {
+      termino: 'EMP y EF',
+      significado: 'Elemento material probatorio y/o evidencia física.',
+    },
+    {
+      termino: 'Embriaguez',
+      significado:
+        'Estado de alteración transitoria de las condiciones físicas y mentales, causada por intoxicación aguda que no permite una adecuada realización de actividades de riesgo.',
+    },
+    {
+      termino: 'Fijación',
+      significado:
+        'Es la forma de registrar el lugar de los hechos para su posterior conocimiento o análisis, al perpetuar con exactitud el aspecto original, lo que permite observar lo ocurrido a pesar de no haber estado presente.',
+    },
+    {
+      termino: 'Indiciado',
+      significado:
+        'Persona objeto de un despliegue de diligencias de averiguación y que no ha asistido a una audiencia de formulación de la imputación.',
+    },
+    {
+      termino: 'Imputado',
+      significado:
+        'Persona quien se responsabiliza de una conducta que es reconocida como delito y contra quien se direcciona una acusación por esa acción.',
+    },
+    {
+      termino: 'Inspección',
+      significado:
+        'Es la observación objetiva, detallada y metódica del lugar de los hechos o lugar distinto al del hecho que permite descubrir, identificar, fijar, recolectar y embalar técnicamente los EMP y EF bajo los protocolos de cadena de custodia, para demostrar la comisión de una conducta punible (Artículo 213 de la Ley 906 de 2004).',
+    },
+    {
+      termino: 'Planimetría',
+      significado:
+        'Parte de la topografía que se ocupa de representar las superficies terrestres, teniendo en cuenta la proyección del terreno sobre un plano horizontal.',
+    },
+    {
+      termino: 'Punto de amarre',
+      significado:
+        'Se denomina al hecho de referenciar un plano a un punto fijo estable que puede ser artificial, natural o geo-referenciado.',
+    },
+    {
+      termino: 'Topografía',
+      significado:
+        'Es la ciencia que permite la representación gráfica de las características artificiales y naturales de un lugar utilizando la planimetría y altimetría. ',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Ley 769 de 2002. Por la cual se expide el Código Nacional de Tránsito Terrestre y se dictan otras disposiciones.  Diario Oficial 44.893 de Agosto 07 de 2002.',
+      link:
+        'http://www.movilidadbogota.gov.co/web/sites/default/files/ley-769-de-2002-codigo-nacional-de-transito_3704_0.pdf',
+    },
+    {
+      referencia:
+        'Ley 906 de 2004. Código de Procedimiento Penal. Diario Oficial número 45.657 del 31 de agosto de 2004.',
+      link:
+        'https://perso.unifr.ch/derechopenal/assets/files/legislacion/l_20190708_03.pdf',
+    },
+    {
+      referencia:
+        'Ley 1503 de 2011. Por la cual se promueve la formación de hábitos, comportamientos y conductas seguros en la vía y se dictan otras disposiciones. Diario Oficial No. 48.298 de 30 de diciembre de 2011. ',
+      link:
+        'http://www.secretariasenado.gov.co/senado/basedoc/ley_1503_2011.html',
+    },
+    {
+      referencia:
+        'Decreto 1079 de 2015. [Ministerio de transporte].  Por medio del cual se expide el Decreto Único Reglamentario del Sector Transporte. 26 de mayo de 2015.',
+      link:
+        'https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=77889#:~:text=Tiene%20por%20objeto%20la%20ejecuci%C3%B3n,por%20el%20Ministerio%20de%20Transporte.',
+    },
+    {
+      referencia:
+        'Instituto Nacional de Medicina Legal y Ciencias Forenses. (2022). Boletines Estadísticos Mensuales.',
+      link:
+        'https://www.medicinalegal.gov.co/cifras-estadisticas/boletines-estadisticos-mensuales ',
+    },
+    {
+      referencia:
+        'Limpert, R. (1999). Motor Vehicle Accident Reconstruction and Cause Analysis. Lexis Publishing.',
+      link:
+        'www.google.com/search?q=Limpert%2C+R.+(1999).+Motor+Vehicle+Accident+Reconstruction+and+Cause+Analysis.+Fifth+Edition.+C',
+    },
+    {
+      referencia:
+        'Resolución 0011268 de 2012. [Ministerio de Transporte]. . Por la cual se adopta el nuevo informe policial de accidentes de tránsito (IPAT), su Manual de diligenciamiento y se dictan otras disposiciones. 06 de Diciembre de 2012.',
+      link:
+        'www.google.com/search?q=Resolución+0011268+de+2012.&rlz=1C5CHFA_enCO775CO775&oq=Resolución+0011268+de+2012.&aqs=chrome..69i57j0l4.1243j0j7&sourceid=chrome&ie=UTF-8',
+    },
+    {
+      referencia:
+        'Ramos Enriquez, H., & Quiroga Amaya, L. (2017). Investigación de Accidentes de Tránsito en Colombia. Imprenta Nacional.',
+      link:
+        'https://www.policia.gov.co/contenido/investigacion-accidentes-transito-colombia',
+    },
+    {
+      referencia:
+        'Rivani , B., Brougham, D., & Mason, R. (1981). Pedestrian PostImpact Kinematics and Injury Patterns. SAE Technical Paper 811024, 42.',
       link: '',
     },
   ],
@@ -147,24 +316,25 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: '',
-        cargo: '',
-        centro: '',
+        nombre: 'Lino Andrés Quiroga Amaya',
+        cargo: 'Experto temático',
+        centro: 'Centro de Tecnologías del Transporte',
       },
       {
-        nombre: '',
-        cargo: '',
-        centro: '',
+        nombre: 'Diego E. Acevedo Guevara',
+        cargo: 'Diseñador instruccional',
+        centro:
+          'Centro de la Industria, la Empresa y los Servicios - CIES Norte de Santander ',
       },
       {
-        nombre: '',
-        cargo: '',
-        centro: '',
+        nombre: 'Carolina Coca Salazar',
+        cargo: 'Asesor metodológico',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
       },
       {
-        nombre: '',
-        cargo: '',
-        centro: '',
+        nombre: 'Jhon Jairo Rodríguez Pérez',
+        cargo: 'Corrector de estilo ',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
       },
     ],
     desarrolloProducto: [
@@ -175,25 +345,25 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Fabian Zarate',
         cargo: 'Diseño web',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: '',
         cargo: 'Producción audiovisual',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Camilo Andres Bolaño Rey',
         cargo: 'Desarrollo front-end',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: '',
         cargo: 'Validación de diseño y contenido',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
